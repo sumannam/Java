762da95af2600b176a68c2c306310e39ae7d5aa3
smnam-home <sumannam@gmail.com>
2026-01-19 05:23:35 +0900

    불필요한 파일 삭제 및 변경

--- LibraryManagement_v2.0/docs/member-search-index.js
+++
@@ -1,1 +0,0 @@
-memberSearchIndex = [{"p":"<Unnamed>","c":"LibraryMain","l":"LibraryMain()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"LibraryMain","l":"login()"},{"p":"<Unnamed>","c":"LibraryMain","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
